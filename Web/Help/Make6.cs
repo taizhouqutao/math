bebd4cdf73cc7cf0f5e27951673d94c544153fa9
GuEn <guen@Myun.info>
2024-04-24 00:04:07 +0800

    2

--- Web/Help/Make6.cs
+++ Web/Help/Make6.cs
@@ -5,7 +5,7 @@ namespace Web.Help
 {
     public class Make6
     {
-        public static string Make6_1(out List<string> res, int? part1 = null, Random farandom = null) //整十数加减整十数
+        public static string Make6_1(out List<string> res, int? part1 = null, Random farandom = null,string operationString="") //整十数加减整十数
         {
             Random random;
             if (farandom == null)
@@ -19,7 +19,10 @@ namespace Web.Help
 
             // 随机选择加法或减法
             int operation = random.Next(0, 2);
-            string operationString = operation == 0 ? "+" : "-";
+            if(string.IsNullOrEmpty(operationString))
+            {
+                operationString = operation == 0 ? "+" : "-";
+            }
 
             // 生成第一个随机整十数
             int num1 = 0;
@@ -48,6 +51,15 @@ namespace Web.Help
 
             // 输出结果
             return $"{num1 * 10} {operationString} {num2 * 10} = ";
+        }
+
+        public static string Make6_1_R(out List<string> res, int num2, Random farandom) //整十数减整十数,反向
+        {
+            //num2 必须大于等于10 小于等于90
+            int num1 = farandom.Next(num2/10, 10);
+            res = new List<string>() { (num1*10).ToString(), "-", (num2*10).ToString() };
+            // 输出结果
+            return $"{num1*10} - {num2*10} = ";
         }
 
         public static string Make6_2(out List<string> res, int? part1 = null, Random farandom = null) //两位数加一位数、整十数
@@ -177,6 +189,11 @@ namespace Web.Help
             Random random = new Random();
 
             bool pass = false;
+            int ifHasKH=0;
+            if(!string.IsNullOrEmpty(selectifKH))
+            {
+                ifHasKH = random.Next(0, 2);
+            }
             List<string> Chars = new List<string>();
             do
             {
@@ -191,18 +208,62 @@ namespace Web.Help
                     temp = Convert.ToInt32(Chars[0]) - Convert.ToInt32(Chars[2]);
                 }
 
-                if (temp/10 < 1 || temp/10 >= 10)
+                if(ifHasKH==1)//有括号
                 {
-                    Chars.Clear();
-                    continue;
+                    int operation = random.Next(0, 2);
+                    string operationString = operation == 0 ? "+" : "-";
+                    if(operationString=="+")
+                    {
+                        if (temp/10 < 1 || temp/10 >= 10)
+                        {
+                            Chars.Clear();
+                            continue;
+                        }
+                        else
+                        {
+                            List<string> Part2 = new List<string>();
+                            Make6_1(out Part2, temp, random,operationString);
+                            Chars.Insert(0, Part2[2]);
+                            Chars.Insert(1, Part2[1]);
+                            Chars.Insert(2, "(");
+                            Chars.Insert(Chars.Count, ")");
+                            pass = true;
+                        }
+                    }
+                    else
+                    {
+                        if (temp/10 < 1 || temp/10 >= 10)
+                        {
+                            Chars.Clear();
+                            continue;
+                        }
+                        else
+                        {
+                            List<string> Part2 = new List<string>();
+                            Make6_1_R(out Part2, temp, random);
+                            Chars.Insert(0, Part2[0]);
+                            Chars.Insert(1, Part2[1]);
+                            Chars.Insert(2, "(");
+                            Chars.Insert(Chars.Count, ")");
+                            pass = true;
+                        }
+                    }
                 }
                 else
                 {
-                    List<string> Part2 = new List<string>();
-                    Make6_1(out Part2, temp/10, random);
-                    Chars.Add(Part2[1]);
-                    Chars.Add(Part2[2]);
-                    pass = true;
+                    if (temp/10 < 1 || temp/10 >= 10)
+                    {
+                        Chars.Clear();
+                        continue;
+                    }
+                    else
+                    {
+                        List<string> Part2 = new List<string>();
+                        Make6_1(out Part2, temp/10, random);
+                        Chars.Add(Part2[1]);
+                        Chars.Add(Part2[2]);
+                        pass = true;
+                    }
                 }
             } while (pass == false);
             // 输出结果
