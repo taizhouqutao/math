c3fe8c221824654ebd5c5ecfc9416dd53e4d03f0
瞿涛 <qutao@wingtech.com>
2024-04-26 10:16:12 +0800

    释放Book

--- Web/Controllers/HomeController.cs
+++ Web/Controllers/HomeController.cs
@@ -112,104 +112,107 @@ namespace Web.Controllers
                     randomList.Add(Chars);
                 }
             }
-            SXSSFWorkbook book = new SXSSFWorkbook();
-            ISheet sheet1 = book.CreateSheet("Sheet1");
-            int i = 0;int rowIdx = 0;
-            if (Lessons.Contains("5.1"))
+            using(SXSSFWorkbook book = new SXSSFWorkbook())
             {
-                do
+                ISheet sheet1 = book.CreateSheet("Sheet1");
+                int i = 0;int rowIdx = 0;
+                if (Lessons.Contains("5.1"))
                 {
-                    IRow row;
-                    if (i == 0)
+                    do
                     {
-                        row = sheet1.CreateRow(rowIdx);
-                    }
-                    else
-                    {
-                        row = sheet1.GetRow(rowIdx);
-                    }
-                    ICellStyle cellStyle = book.CreateCellStyle();
-                    IFont font = book.CreateFont();
-                    font.FontHeightInPoints = 14; // 设置字体大小，单位为点
-                    cellStyle.SetFont(font);
-
-                    row.Height = 600;
-
-                    var cell = row.CreateCell(i * 2);
-                    var cellValue = randomList[0];
-                    cell.SetCellValue(cellValue);
-                    cell.CellStyle = cellStyle;
-                    var cellAns = row.CreateCell(i * 2 + 1);
-                    int Width = 9000;
-                    sheet1.SetColumnWidth(i * 2 + 1, Width);
-                    cellAns.CellStyle = cellStyle;
-                    i = i + 1;
-                    if (i == 2)
-                    {
-                        i = 0;
-                        rowIdx = rowIdx + 1;
-                    }
-                    randomList.RemoveAt(0);
-                } while (randomList.Count > 0);
-            }
-            else
-            {
-                do
-                {
-                    IRow row;
-                    if (i == 0)
-                    {
-                        row = sheet1.CreateRow(rowIdx);
-                    }
-                    else
-                    {
-                        row = sheet1.GetRow(rowIdx);
-                    }
-                    ICellStyle cellStyle = book.CreateCellStyle();
-                    IFont font = book.CreateFont();
-                    font.FontHeightInPoints = 14; // 设置字体大小，单位为点
-                    cellStyle.SetFont(font);
-
-                    row.Height = 600;
-
-                    var cell = row.CreateCell(i * 2);
-                    var cellValue = randomList[0];
-                    if (!string.IsNullOrEmpty(selectifMany))
-                    {
-                        cellValue = cellValue.Replace(" ", "");
-                    }
-                    cell.SetCellValue(cellValue);
-                    cell.CellStyle = cellStyle;
-                    var cellAns = row.CreateCell(i * 2 + 1);
-                    int Width = 2400;
-                    if (!string.IsNullOrEmpty(selectifMany))
-                    {
-                        if (i == 3 || i == 4)
+                        IRow row;
+                        if (i == 0)
                         {
-                            Width = 2850;
+                            row = sheet1.CreateRow(rowIdx);
                         }
                         else
                         {
-                            Width = 2100;
+                            row = sheet1.GetRow(rowIdx);
                         }
-                    }
-                    sheet1.SetColumnWidth(i * 2 + 1, Width);
-                    cellAns.CellStyle = cellStyle;
-                    i = i + 1;
-                    if (i == 5)
+                        ICellStyle cellStyle = book.CreateCellStyle();
+                        IFont font = book.CreateFont();
+                        font.FontHeightInPoints = 14; // 设置字体大小，单位为点
+                        cellStyle.SetFont(font);
+
+                        row.Height = 600;
+
+                        var cell = row.CreateCell(i * 2);
+                        var cellValue = randomList[0];
+                        cell.SetCellValue(cellValue);
+                        cell.CellStyle = cellStyle;
+                        var cellAns = row.CreateCell(i * 2 + 1);
+                        int Width = 9000;
+                        sheet1.SetColumnWidth(i * 2 + 1, Width);
+                        cellAns.CellStyle = cellStyle;
+                        i = i + 1;
+                        if (i == 2)
+                        {
+                            i = 0;
+                            rowIdx = rowIdx + 1;
+                        }
+                        randomList.RemoveAt(0);
+                    } while (randomList.Count > 0);
+                }
+                else
+                {
+                    do
                     {
-                        i = 0;
-                        rowIdx = rowIdx + 1;
-                    }
-                    randomList.RemoveAt(0);
-                } while (randomList.Count > 0);
+                        IRow row;
+                        if (i == 0)
+                        {
+                            row = sheet1.CreateRow(rowIdx);
+                        }
+                        else
+                        {
+                            row = sheet1.GetRow(rowIdx);
+                        }
+                        ICellStyle cellStyle = book.CreateCellStyle();
+                        IFont font = book.CreateFont();
+                        font.FontHeightInPoints = 14; // 设置字体大小，单位为点
+                        cellStyle.SetFont(font);
+
+                        row.Height = 600;
+
+                        var cell = row.CreateCell(i * 2);
+                        var cellValue = randomList[0];
+                        if (!string.IsNullOrEmpty(selectifMany))
+                        {
+                            cellValue = cellValue.Replace(" ", "");
+                        }
+                        cell.SetCellValue(cellValue);
+                        cell.CellStyle = cellStyle;
+                        var cellAns = row.CreateCell(i * 2 + 1);
+                        int Width = 2400;
+                        if (!string.IsNullOrEmpty(selectifMany))
+                        {
+                            if (i == 3 || i == 4)
+                            {
+                                Width = 2850;
+                            }
+                            else
+                            {
+                                Width = 2100;
+                            }
+                        }
+                        sheet1.SetColumnWidth(i * 2 + 1, Width);
+                        cellAns.CellStyle = cellStyle;
+                        i = i + 1;
+                        if (i == 5)
+                        {
+                            i = 0;
+                            rowIdx = rowIdx + 1;
+                        }
+                        randomList.RemoveAt(0);
+                    } while (randomList.Count > 0);
+                }
+                var ms = new NpoiMemoryStream();
+                ms.AllowClose = false;
+                book.Write(ms);
+                sheet1=null;
+                ms.Seek(0, SeekOrigin.Begin);
+                ms.AllowClose = true;
+                return File(ms, "application/vnd.ms-excel","口算题.xls"); 
             }
-            var ms = new NpoiMemoryStream();
-            ms.AllowClose = false;
-            book.Write(ms);
-            ms.Seek(0, SeekOrigin.Begin);
-            ms.AllowClose = true;
-            return File(ms, "application/vnd.ms-excel","口算题.xls"); 
         }
 
         public static List<T> GetRandomSubset<T>(List<T> source, int n)
