5df57c157585b27c80e322244effc651d29ef1aa
瞿涛 <qutao@wingtech.com>
2024-05-24 14:24:08 +0800

    添加二年级下

---
+++ Web/Controllers/Grade2_1Controller.cs
@@ -0,0 +1,104 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Web.Models;
+using System.IO;
+using NPOI.XSSF.Streaming;
+using NPOI.SS.UserModel;
+using Web.Help;
+
+namespace Web.Controllers
+{
+    public class  Grade2_1Controller: Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ExportExcel(string selectItem,string itemcount,string selectifMany,string selectifKH)
+        {
+            if(string.IsNullOrEmpty(selectItem)){
+                return RedirectToAction("Index");
+            }
+            var Lessons = selectItem.Split(",");
+            var clientIp = HttpContext.Connection.RemoteIpAddress.ToString();
+            var guid = Guid.NewGuid().ToString();
+            Console.WriteLine($"1:IP:{clientIp} Request:{DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss")} Guid:{guid}");
+            decimal.TryParse(itemcount, out decimal totalst);
+            if (totalst == 0) totalst = 100;
+            int total = Convert.ToInt32(Math.Floor(totalst));
+            List<string> randomList = new List<string>();
+            Random rd = new Random();
+            for (int indx = 0; indx < total; indx++)
+            {
+                List<string> OutArray = new List<string>();
+                var indexKey = rd.Next(0, Lessons.Count());
+                var unit = Lessons[indexKey];
+                if (unit == "6.1")
+                {
+                    int chlimit = 0;
+                    if (!string.IsNullOrEmpty(selectifMany))
+                    {
+                        chlimit = indx % 5;
+                    }
+                    var Chars = (chlimit == 3 || chlimit == 4) ? Make6.Make6_1_Sev(selectifKH) : Make6.Make6_1(out OutArray);
+                    randomList.Add(Chars);
+                }
+                else if (unit == "6.2")
+                {
+                    int chlimit = 0;
+                    if (!string.IsNullOrEmpty(selectifMany))
+                    {
+                        chlimit = indx % 5;
+                    }
+                    var Chars = (chlimit == 3 || chlimit == 4) ? Make6.Make6_2_Sev(selectifKH) : Make6.Make6_2(out OutArray);
+                    randomList.Add(Chars);
+                }
+                else if (unit == "6.3")
+                {
+                    int chlimit = 0;
+                    if (!string.IsNullOrEmpty(selectifMany))
+                    {
+                        chlimit = indx % 5;
+                    }
+                    var Chars = (chlimit == 3 || chlimit == 4) ? Make6.Make6_3_Sev(selectifKH) : Make6.Make6_3(out OutArray);
+                    randomList.Add(Chars);
+                }
+                else if (unit == "2.2")
+                {
+                    int chlimit = 0;
+                    if (!string.IsNullOrEmpty(selectifMany))
+                    {
+                        chlimit = indx % 5;
+                    }
+                    var Chars = (chlimit == 3 || chlimit == 4) ? Make2.Make2_2_Sev(selectifKH) : Make2.Make2_2(out OutArray);
+                    randomList.Add(Chars);
+                }
+                else if (unit == "2.1")
+                {
+                    int chlimit = 0;
+                    if (!string.IsNullOrEmpty(selectifMany))
+                    {
+                        chlimit = indx % 5;
+                    }
+                    var Chars = (chlimit == 3 || chlimit == 4) ? Make2.Make2_1_Sev(selectifKH) : Make2.Make2_1(out OutArray);
+                    randomList.Add(Chars);
+                }
+                else if(unit=="5.1")
+                {
+                    var Chars = Make5.Make5_1();
+                    randomList.Add(Chars);
+                }
+            }
+            Console.WriteLine($"2:IP:{clientIp} Make:{DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss")} Guid:{guid}");
+            var ms = MakeFile.GetFileStream(Lessons,randomList,selectifMany);
+            Console.WriteLine($"3:IP:{clientIp} OutPut:{DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss")} Guid:{guid}");
+            return File(ms, "application/vnd.ms-excel","口算题.xls"); 
+        }
+    }
+}
---
+++ Web/Help/MakeFile.cs
@@ -0,0 +1,141 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Web.Models;
+using System.IO;
+using NPOI.XSSF.Streaming;
+using NPOI.SS.UserModel;
+using Web.Help;
+
+namespace Web.Help
+{
+    public static class MakeFile
+    {
+        public static NpoiMemoryStream GetFileStream(string[] Lessons,List<string> randomList,string selectifMany)
+        {
+            using(SXSSFWorkbook book = new SXSSFWorkbook())
+            {
+                ISheet sheet1 = book.CreateSheet("Sheet1");
+                int i = 0;int rowIdx = 0;
+                if (Lessons.Contains("5.1"))
+                {
+                    do
+                    {
+                        IRow row;
+                        if (i == 0)
+                        {
+                            row = sheet1.CreateRow(rowIdx);
+                        }
+                        else
+                        {
+                            row = sheet1.GetRow(rowIdx);
+                        }
+                        ICellStyle cellStyle = book.CreateCellStyle();
+                        IFont font = book.CreateFont();
+                        font.FontHeightInPoints = 14; // 设置字体大小，单位为点
+                        cellStyle.SetFont(font);
+
+                        row.Height = 600;
+
+                        var cell = row.CreateCell(i * 2);
+                        var cellValue = randomList[0];
+                        cell.SetCellValue(cellValue);
+                        cell.CellStyle = cellStyle;
+                        var cellAns = row.CreateCell(i * 2 + 1);
+                        int Width = 9000;
+                        sheet1.SetColumnWidth(i * 2 + 1, Width);
+                        cellAns.CellStyle = cellStyle;
+                        i = i + 1;
+                        if (i == 2)
+                        {
+                            i = 0;
+                            rowIdx = rowIdx + 1;
+                        }
+                        randomList.RemoveAt(0);
+                    } while (randomList.Count > 0);
+                }
+                else
+                {
+                    do
+                    {
+                        IRow row;
+                        if (i == 0)
+                        {
+                            row = sheet1.CreateRow(rowIdx);
+                        }
+                        else
+                        {
+                            row = sheet1.GetRow(rowIdx);
+                        }
+                        ICellStyle cellStyle = book.CreateCellStyle();
+                        IFont font = book.CreateFont();
+                        font.FontHeightInPoints = 14; // 设置字体大小，单位为点
+                        cellStyle.SetFont(font);
+
+                        row.Height = 600;
+
+                        var cell = row.CreateCell(i * 2);
+                        var cellValue = randomList[0];
+                        if (!string.IsNullOrEmpty(selectifMany))
+                        {
+                            cellValue = cellValue.Replace(" ", "");
+                        }
+                        cell.SetCellValue(cellValue);
+                        cell.CellStyle = cellStyle;
+                        var cellAns = row.CreateCell(i * 2 + 1);
+                        int Width = 2400;
+                        if (!string.IsNullOrEmpty(selectifMany))
+                        {
+                            if (i == 3 || i == 4)
+                            {
+                                Width = 2850;
+                            }
+                            else
+                            {
+                                Width = 2100;
+                            }
+                        }
+                        sheet1.SetColumnWidth(i * 2 + 1, Width);
+                        cellAns.CellStyle = cellStyle;
+                        i = i + 1;
+                        if (i == 5)
+                        {
+                            i = 0;
+                            rowIdx = rowIdx + 1;
+                        }
+                        randomList.RemoveAt(0);
+                    } while (randomList.Count > 0);
+                }
+                var ms = new NpoiMemoryStream();
+                ms.AllowClose = false;
+                book.Write(ms);
+                sheet1=null;
+                ms.Seek(0, SeekOrigin.Begin);
+                ms.AllowClose = true;
+                return ms; 
+            }
+        }
+
+        public class NpoiMemoryStream: MemoryStream
+        {
+            public NpoiMemoryStream()
+            {
+                AllowClose = true;
+            }
+
+            public bool AllowClose { get; set; }
+
+            public override void Close()
+            {
+                if(AllowClose)
+                {
+                    base.Close();
+                }
+            }
+        }
+    }
+}
